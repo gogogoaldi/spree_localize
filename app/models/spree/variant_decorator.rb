f6e1cf86e822d5b1704dbef71009278366f487d7
Olaf Tiemann <olaf@faserleuchten.de>
2012-05-09 22:46:56 +0200

    added old_price to normalization

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -3,7 +3,7 @@ Spree::Variant.class_eval do
     write_attribute(:price, SpreeLocalize.normalize_number(value))
   end
 
-  [:cost_price, :weight, :height, :width, :depth].each do |attribute|
+  [:old_price, :cost_price, :weight, :height, :width, :depth].each do |attribute|
     define_method("#{attribute}=") do |value|
       write_attribute(attribute, SpreeLocalize.normalize_number(value))
     end
